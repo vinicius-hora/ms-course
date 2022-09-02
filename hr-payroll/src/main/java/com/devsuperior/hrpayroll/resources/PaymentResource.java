d9cf8d6085882f8f45260af33e0f47c3334bf5d4
vinicius-hora <vinicius_hora@live.com>
2022-09-02 09:35:15 -0300

    hystrix

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -2,7 +2,10 @@ package com.devsuperior.hrpayroll.resources;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.exception.ExceptionNotWrappedByHystrix;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +19,18 @@ public class PaymentResource {
 
     private final PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
 
     }
+
+    public ResponseEntity<Payment> getPaymentAlternative(@PathVariable Long workerId, @PathVariable Integer days){
+        Payment payment = new Payment("Bram", 400.0, days);
+        return ResponseEntity.ok(payment);
+
+    }
+
 }
