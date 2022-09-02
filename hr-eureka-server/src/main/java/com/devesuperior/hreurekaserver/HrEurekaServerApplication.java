896715aa38dc47a72633a07ca436ccf1f29f8c60
vinicius-hora <vinicius_hora@live.com>
2022-09-02 00:36:56 -0300

    configure eureka clients

--- hr-eureka-server/src/main/java/com/devesuperior/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/devesuperior/hreurekaserver/HrEurekaServerApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+
 @SpringBootApplication
+@EnableEurekaServer
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
