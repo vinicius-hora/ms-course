78388fb100a12030bf33ae1ba03d22c7e50bdc9c
vinicius-hora <vinicius_hora@live.com>
2022-09-02 09:45:49 -0300

    api-gatway-zull created

---
+++ hr-api-gatway-zuul/src/main/java/com/devsuperior/hrapigatwayzuul/HrApiGatwayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrapigatwayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatwayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatwayZuulApplication.class, args);
+	}
+
+}
---
+++ hr-api-gatway-zuul/src/test/java/com/devsuperior/hrapigatwayzuul/HrApiGatwayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrapigatwayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatwayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
